b55868141c81de2fb9f896788532cdafbb88f305
Camille Walters <camille.h.walters@gmail.com>
2023-05-12 10:09:37 -0400

    Use new input system instead oopsies

--- Eggstravaganza/Assets/PlayerMovement.cs
+++ Eggstravaganza/Assets/PlayerMovement.cs
@@ -1,43 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public Transform body;
 
+    public PlayerInputActions playerControls;
+
+    private InputAction move;
+    private InputAction interact;
+    private InputAction look;
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+
+        interact = playerControls.Player.Fire;
+        interact.Enable();
+        interact.performed += Interact;
+
+        look = playerControls.Player.Look;
+        look.Enable();
+
+    }
+    private void OnDisable()
+    {
+        move.Disable();
+        interact.Disable();
+        look.Disable();
+    }
+
     private void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(horizontal, 0f, vertical);//this might be off
+        Vector2 movementDirection = move.ReadValue<Vector2>();
+        Vector3 movement = new Vector3(movementDirection.x, 0f, movementDirection.y);
         movement.Normalize();
 
+        // Move the player
         transform.Translate(movement * speed * Time.deltaTime);
 
-        ////turn towards direction of movement, might not want this? idk really how it works?
-        //if (movement != Vector3.zero)
-        //{
-        //    Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
-        //    body.rotation = Quaternion.Lerp(body.rotation, toRotation, Time.deltaTime * 10f);
-        //    //Debug.Log("rotate: " + body.rotation);
-        //}
+        Vector2 lookDirection = look.ReadValue<Vector2>();
+        Vector3 rotation = new Vector3(lookDirection.x, 0f, lookDirection.y);
+        rotation.Normalize();
 
-        float shootHorizontal = Input.GetAxis("Mouse X");
-        float shootVertical = Input.GetAxis("Mouse Y");
-
-        Vector3 shootingDirection = new Vector3(shootHorizontal, 0f, shootVertical);
-        if (shootingDirection != Vector3.zero)
+        //Rotate the player
+        if (rotation != Vector3.zero)
         {
-            Quaternion toRotation = Quaternion.LookRotation(shootingDirection, Vector3.up);
+            Quaternion toRotation = Quaternion.LookRotation(rotation, Vector3.up);
             body.rotation = Quaternion.Lerp(body.rotation, toRotation, Time.deltaTime * 10f);
         }
 
-        if (Input.GetButton("Fire1"))//this is click on the mouse. could change later too
-        {
-            Debug.Log("picked up egg");
-        }
     }
+
+    private void Interact (InputAction.CallbackContext context)
+    {
+        Debug.Log("we interacted");//whatever picking up/throwing egg logic we want here
+    }
+
 }
