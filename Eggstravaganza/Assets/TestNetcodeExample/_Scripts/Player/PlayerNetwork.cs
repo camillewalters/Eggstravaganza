d9f8de61c1f7af16cb2e53ad4dde8fc9e3a80b47
Jaden Goter <jadengoter@gmail.com>
2023-05-12 17:32:21 -0700

    Add balls

--- Eggstravaganza/Assets/TestNetcodeExample/_Scripts/Player/PlayerNetwork.cs
+++ Eggstravaganza/Assets/TestNetcodeExample/_Scripts/Player/PlayerNetwork.cs
@@ -3,6 +3,36 @@ using UnityEngine;
 
 public class PlayerNetwork : NetworkBehaviour
 {
+    [SerializeField] private float m_InterpolationTime = 0.1f;
+
+    private readonly NetworkVariable<PlayerNetworkData> m_NetState = new(writePerm: NetworkVariableWritePermission.Owner);
+
+    private Vector3 m_Velocity;
+    private float m_RotationVelocity;
+
+    private void Update()
+    {
+        if (IsOwner)
+        {
+            m_NetState.Value = new PlayerNetworkData
+            {
+                Position = transform.position,
+                Rotation = transform.rotation.eulerAngles
+            };
+        }
+        else
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, m_NetState.Value.Position, ref m_Velocity,
+                m_InterpolationTime);
+            transform.rotation = Quaternion.Euler(
+                0f,
+                Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, m_NetState.Value.Rotation.y,
+                    ref m_RotationVelocity, m_InterpolationTime),
+                0f);
+        }
+    }
+
+
     private struct PlayerNetworkData : INetworkSerializable
     {
         private float m_XPos;
@@ -30,33 +60,6 @@ public class PlayerNetwork : NetworkBehaviour
             serializer.SerializeValue(ref m_XPos);
             serializer.SerializeValue(ref m_ZPos);
             serializer.SerializeValue(ref m_YRot);
-        }
-    }
-
-    [SerializeField] private float m_InterpolationTime = 0.1f;
-
-    private readonly NetworkVariable<PlayerNetworkData> m_NetState = new(writePerm: NetworkVariableWritePermission.Owner);
-
-    private Vector3 m_Velocity;
-    private float m_RotationVelocity;
-
-    private void Update()
-    {
-        if (IsOwner)
-        {
-            m_NetState.Value = new PlayerNetworkData
-            {
-                Position = transform.position,
-                Rotation = transform.rotation.eulerAngles
-            };
-        }
-        else
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, m_NetState.Value.Position, ref m_Velocity, m_InterpolationTime);
-            transform.rotation = Quaternion.Euler(
-                0f,
-                Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y, m_NetState.Value.Rotation.y, ref m_RotationVelocity, m_InterpolationTime),
-                0f);
         }
     }
 }
