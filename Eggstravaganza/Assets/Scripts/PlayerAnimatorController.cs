bb65ed041704e6100090547ebcc85738c4679616
Amanda Trang <amanda.trang@unity3d.com>
2023-05-18 14:45:14 -0700

    Throw anim

--- Eggstravaganza/Assets/Scripts/PlayerAnimatorController.cs
+++ Eggstravaganza/Assets/Scripts/PlayerAnimatorController.cs
@@ -11,7 +11,7 @@ public class PlayerAnimatorController : MonoBehaviour
     Rigidbody m_Rigidbody;
     Animator m_Animator;
     static readonly int IsWalking = Animator.StringToHash("isWalking");
-    static readonly int Throw = Animator.StringToHash("throw");
+    static readonly int IsThrow = Animator.StringToHash("throw");
 
     // Start is called before the first frame update
     void Start()
@@ -25,25 +25,16 @@ public class PlayerAnimatorController : MonoBehaviour
     {
         var isMoving = m_Rigidbody.velocity != Vector3.zero;
         m_Animator.SetBool(IsWalking, isMoving);
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            m_Animator.SetTrigger(Throw);
-        }
-        
-        // DEBUG
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            // StartCoroutine(ConfettiParticles());
-            ParticleSystem.Stop();
-            ParticleSystem.Play();
-        }
     }
 
-    IEnumerator ConfettiParticles()
+    public void Throw()
+    {
+        m_Animator.SetTrigger(IsThrow);
+    }
+
+    public void Confetti()
     {
         ParticleSystem.Stop();
         ParticleSystem.Play();
-        yield return new WaitForSeconds(5f);
     }
 }
