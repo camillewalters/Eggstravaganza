6bb93a4d4f511e8e22b029e450639b4a4a957724
Camille Walters <camille.h.walters@gmail.com>
2023-05-19 12:55:56 -0400

    Egg fix?

---
+++ Eggstravaganza/Assets/Scripts/EggBehaviorNetworked.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Unity.Netcode;
+
+public class EggBehaviorNetworked: NetworkBehaviour
+{
+	public bool isBeingHeld = false;
+	public bool isBeingThrown = false;
+
+    Rigidbody rb;
+
+    public Object thrownBy;
+    //public PlayerControllerNetworked thrownByNet;
+    public Object droppedBy;
+
+    private void Awake()
+    {
+         rb = GetComponent<Rigidbody>();
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner) Destroy(this);
+    }
+
+    private void Update()
+    {
+        rb.detectCollisions = !isBeingHeld;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.GetComponent<PlayerController>() == null)
+        {
+            isBeingThrown = false;
+            isBeingHeld = false;
+            droppedBy = null;
+        }
+    }
+}
---
+++ Eggstravaganza/Assets/Scripts/EggNetwork.cs
@@ -0,0 +1,39 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Unity.Netcode;
+
+//public class EggNetwork : NetworkBehaviour
+//{
+//    private readonly NetworkVariable
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (IsOwner)
+//        {
+
+//        }
+//    }
+
+//    private struct EggNetworkData : INetworkSerializable
+//    {
+//        private bool isBeingHeld;
+//        private bool isBeingThrown;
+
+//        internal EggBehavior eb
+//        {
+//            get => new()
+//        }
+
+//        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+//        {
+
+//        }
+//    }
+//}
